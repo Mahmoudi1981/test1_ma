5bf445ca085494c2c7370577e92315bf7aee5dbf
mostafa <114899876+gg2dev@users.noreply.github.com>
2023-10-28 12:47:08 +0330

    edit
    edit1

--- client.py
+++ client.py
@@ -1,8 +1,43 @@
 import socket
-ip = "127.0.0.1"
-port = 5230
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((ip , port))
-data = s.recv(1024)
-print(data)
-s.close()
+import threading
+
+def receive_messages(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode()
+            process_message(message)
+        except socket.error:
+            print("An error occurred while receiving the message.")
+            client_socket.close()
+            break
+
+def process_message(message):
+    if message:
+        sender_index = message.index(":")
+        sender = message[1:sender_index]
+        print(f"{sender}: {message[sender_index+2:]}")
+
+def send_message(client_socket):
+    while True:
+        message = input()
+        client_socket.send(message.encode())
+
+def main():
+    host = '127.0.0.1'  # آدرس IP سرور
+    port = 1234  # پورت سرور
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client_socket.connect((host, port))
+    except socket.error:
+        print("Unable to connect to the server.")
+        return
+
+    print("Connected to server.")
+    username = input("Enter your username: ")
+
+    threading.Thread(target=receive_messages, args=(client_socket,)).start()
+    threading.Thread(target=send_message, args=(client_socket,)).start()
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -1,11 +1,52 @@
 import socket
-tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpSocket.bind(('127.0.0.1', 5230))
-tcpSocket.listen(5)
+import threading
+import getpass
+
+def broadcast_messages(message, exclude_client=None):
+    for client_socket in client_sockets:
+        if client_socket != exclude_client:
+            try:
+                message = f"[{username}]: {message}"
+                client_socket.send(message.encode())
+            except socket.error:
+                print("An error occurred while sending the message to a client.")
+                client_sockets.remove(client_socket)
 
 
-while True:
-    client, addr = tcpSocket.accept()
-    print('Received connection from',addr)
-    client.send(b'thank you for connecting')
-    client.close()
+def handle_client(client_socket, client_address):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode()
+        except socket.error:
+            print(f"Error occurred while receiving message from {client_address}.")
+            client_sockets.remove(client_socket)
+            break
+
+        if message:
+            print(f"Received message: {message}")
+            broadcast_messages(message, exclude_client=client_socket)
+
+    client_socket.close()
+
+def main():
+    host = '127.0.0.1'  # آدرس IP سرور
+    port = 1234  # پورت سرور
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(5)
+
+    print("Server started. Waiting for clients to connect...")
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Client {client_address} connected.")
+
+        client_sockets.append(client_socket)
+
+        threading.Thread(target=handle_client, args=(client_socket, client_address)).start()
+
+if __name__ == '__main__':
+    username = getpass.getuser()  # نام کاربری فعلی که در اجرای کد استفاده شده است.
+    client_sockets = []
+    main()
